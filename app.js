9ca347d49875bf9a6e7adb39d7534e7224e2a8d9
Leandro Cabranes <leandro.cabranes@c-tech.com.ar>
2020-12-26 14:41:57 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.json({message: 'Hola Mundo v2'});
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Runing on http://${HOST}:${PORT}`);
